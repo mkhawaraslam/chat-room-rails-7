60b32a4cad5ab7a30d47f119988b0ee4ea965ae3
mkhawaraslam <mkhawaraslam0@gmail.com>
2023-03-03 16:40:24 +0500

    db seed

--- db/seeds/00_users.rb
+++ db/seeds/00_users.rb
@@ -3,7 +3,7 @@
 puts 'Running Seeds...'
 users = [
   { email: 'john.doe@example.com', first_name: 'John', last_name: 'Doe', password: 'password' },
-  { email: 'chris.oliver@example.com', first_name: 'Chris', last_name: 'Oliver', password: 'password' }
+  { email: 'khawar.aslam@example.com', first_name: 'Khawar', last_name: 'Aslam', password: 'password' }
 ]
 users.each do |attrs|
   new_user = User.find_or_initialize_by(email: attrs[:email])
